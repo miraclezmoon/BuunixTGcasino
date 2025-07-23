e04cb492b236253e4ccbeaf6c67499a03dfcc0c7
miraclezmoon <miraclezmoon@gmail.com>
2025-07-23 13:29:04 -0700

    Switched from better-sqlite3 to sqlite3 for Railway compatibility

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,7 @@
+
 import { drizzle } from 'drizzle-orm/sqlite3';
 import sqlite3 from 'sqlite3';
+import * as schema from './schema';
 
 const sqlite = new sqlite3.Database('data.db');
+export const db = drizzle(sqlite, { schema });
